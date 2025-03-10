1a4207b5cb0d204028adeac358740e1871ca4cba
GizakiF <142325813+GizakiF@users.noreply.github.com>
2025-03-10 12:07:14 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -15,43 +15,22 @@ svm_path = os.path.expanduser(
 pca_path = os.path.expanduser(
     "~/Research/Sleep Deprivation Detection using voice/output/pop_level/population_level_pca.pkl"
 )
-# test_sample_path = os.path.expanduser(
-#     "~/Research/Sleep Deprivation Detection using voice/dataset/osf/stmtf/strf_session_post_subjectNb_01_daySession_03_segmentNb_1.pkl"
-# )
-#
 test_sample_path = os.path.expanduser(
-    "~/github/modified-strf-like-model/test_strf.pkl")
+    "~/Research/Sleep Deprivation Detection using voice/dataset/osf/stmtf/strf_session_post_subjectNb_01_daySession_03_segmentNb_1.pkl"
+)
 
 with open(svm_path, "rb") as f:
     svm = pickle.load(f)
 with open(pca_path, "rb") as f:
     pca = pickle.load(f)
 
+# test_sample = pickle.load(test_sample_path)
 with open(test_sample_path, "rb") as f:
-    test_sample_batch = pickle.load(f)
+    test_sample = pickle.load(f)
 
-print(f"Type of test_sample_batch: {type(test_sample_batch)}")
-print(f"Shape of test_sample_batch: {test_sample_batch.shape}")
-print(f"Data type of test_sample_batch: {test_sample_batch.dtype}")
-
-sample_index_to_predict = 0
-test_sample = test_sample_batch[sample_index_to_predict]
-
-print(
-    f"\nPredicting for a single sample (index {
-        sample_index_to_predict
-    }) from the batch:"
-)
-print(f"Shape of single test_sample: {test_sample.shape}")
-
-test_sample_flattened = test_sample.flatten()  # Flatten the *single sample*
-test_sample_reshaped = test_sample_flattened.reshape(1, -1)
-test_sample = test_sample_reshaped
-
-# absolute complex nubers to make it real num
-test_sample_real = np.abs(test_sample)
-test_sample = test_sample_real
-
+# original_shape = test_sample.shape
+test_sample_flattened = np.asarray(test_sample["strf"]).flatten()
+test_sample = test_sample_flattened.reshape(1, -1)
 
 expected_features = pca.components_.shape[1]
 if test_sample_flattened.shape[0] != expected_features:
@@ -68,6 +47,9 @@ else:
     test_sample_normalized = test_sample
 
 test_sample_pca = pca.transform(test_sample_normalized)
+
+
+# X_pca = pca.transform(test_sample_flattened.reshape(1, -1))
 
 y_pred = svm.predict(test_sample_pca)
 
